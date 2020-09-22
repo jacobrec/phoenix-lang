782fdd50d486f3651825ec957b2735d754d13202
Jacob Reckhard <jacobreckhard@gmail.com>
2020-09-22 15:17:14 -0600

    added standard library

--- interpreter/bin/main.ml
+++ interpreter/bin/main.ml
@@ -5,5 +5,6 @@ let () =
   let onfilename f =
     didFile := true;
     Driver.parse_and_eval_file f in
+  Driver.load_lib ();
   Arg.parse [] onfilename "";
   if !didFile then () else Driver.repl ()
