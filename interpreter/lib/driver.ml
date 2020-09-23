821daaea858f7dfacf59fc4b8c69ee3d684c50fa
Jacob Reckhard <jacobreckhard@gmail.com>
2020-09-23 11:25:39 -0600

    repl doesn't crash on syntax error

--- interpreter/lib/driver.ml
+++ interpreter/lib/driver.ml
@@ -1,10 +1,13 @@
-let parse_and_eval ?(loud=false) ?(filename="repl") filebuf =
-  let parsed = Parse_runner.parse ~loud ~filename filebuf in
-  Eval.evaluate ~loud parsed
+let parse_and_eval ?(loud=false) ?(filename="repl") ?(exit_on_error=true) filebuf =
+  let parsed = Parse_runner.parse ~loud ~filename ~exit_on_error filebuf in
+  match parsed with
+  | Some parsed -> Eval.evaluate ~loud parsed
+  | None -> ()
 
-let parse_and_eval_string ?(loud=false) str = 
+
+let parse_and_eval_string str = 
   let filebuf = Lexing.from_string str in
-  parse_and_eval ~loud filebuf
+  parse_and_eval ~loud:true ~exit_on_error:false filebuf
 
 let parse_and_eval_file path = 
   let ch = open_in path in
@@ -17,5 +20,5 @@ let load_lib _ =
 let rec repl _ =
   print_string "> ";
   flush stdout;
-  parse_and_eval_string ~loud:true (input_line stdin);
+  parse_and_eval_string (input_line stdin);
   repl ()
--- interpreter/lib/parse_runner.ml
+++ interpreter/lib/parse_runner.ml
@@ -6,20 +6,22 @@ let print_position outx lexbuf =
   Printf.fprintf outx "%s:%d:%d" pos.pos_fname
     pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
 
-let parse_with_error lexbuf =
-  try Parser.main Lexer.token lexbuf with
+let parse_with_error exit_on_error lexbuf =
+  try Some (Parser.main Lexer.token lexbuf) with
   | SyntaxError msg ->
-     Printf.fprintf stderr "%a: %s\n" print_position lexbuf msg;
-     exit (-1)
+     Printf.printf "%a: %s\n" print_position lexbuf msg;
+     if exit_on_error then exit (-1) else None
   | Parser.Error ->
-     Printf.fprintf stderr "%a: syntax error\n" print_position lexbuf;
-     exit (-1)
+     Printf.printf "%a: syntax error\n" print_position lexbuf;
+     if exit_on_error then exit (-1) else None
 
-let parse ?(loud=false) ?(filename="repl") lexbuf = 
+let parse ?(loud=false) ?(filename="repl") ?(exit_on_error=true) lexbuf = 
   lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = filename };
-  let parsed = parse_with_error lexbuf in
+  let parsed = parse_with_error exit_on_error lexbuf in
   if loud then begin
   print_string "// Parsed to: ";
-  print_endline (String.concat "" (List.map Ast.string_of_stmt parsed));
+  match parsed with
+  | Some parsed -> print_endline (String.concat "" (List.map Ast.string_of_stmt parsed))
+  | None -> print_endline "error"
   end else ();
   parsed
